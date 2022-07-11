a10c8e04d4155e315779ead5bc71e0f9524f82d0
LeeVaughn <dlvprogramming@gmail.com>
2022-07-11 10:48:26 -0700

    Update file name

--- script.py
+++ script.py
@@ -12,7 +12,7 @@ def convert_wav(file):
     # skips .DS_Store file
     if not file.endswith(".DS_Store"):
         # creates a .wav file
-        check_output(f'ffmpeg -i ./audio/{file} ./audio-wav/{file_name}.wav', shell=True)
+        check_output(f'ffmpeg -i ./audio/{file} ./audio_wav/{file_name}.wav', shell=True)
         # creates a new file with a sample rate of 8000 khz
         check_output(f'sox ./audio-wav/{file_name}.wav -r 8000 ./transcoded_audio/{file_name}.8khz.wav', shell=True)
 
